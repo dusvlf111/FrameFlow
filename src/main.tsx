90f265d742c48b2e2cdb7dc32297f3ba49588579
sungbin <dusvlf5950@naver.com>
2025-07-27 20:30:06 +0900

    feat: add Vercel analytics and speed insights to the application

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import './index.css'
 import './i18n'; // i18n 설정 임포트
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </I18nextProvider>
   </React.StrictMode>,
 )
